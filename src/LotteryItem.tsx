532004f8b8ec3beeea62a94b76052f7af9b0d711
pingxiaodou <604436810@qq.com>
2019-08-22 15:28:23 +0800

    Update LotteryItem.tsx

--- src/LotteryItem.tsx
+++ src/LotteryItem.tsx
@@ -5,54 +5,70 @@ import {
   ImageBackground,
   StyleSheet,
   TouchableHighlight,
+  Text,
+  TouchableOpacity
 } from 'react-native';
 
 const ItemStyle = StyleSheet.create({
   container: {
-    
+
   },
   highLightBlock: {
-      backgroundColor: 'rgba(0, 0, 0, .5)',
-      borderRadius: 10,
+    backgroundColor: 'rgba(50,50,50,0.5)',
+    borderRadius: 4,
+    position: 'absolute',
+    top: 0,
+    left: 0
   },
 });
 
 interface Props {
-  url: string;
+  url: any;
   index: number;
   lotteryPress?(): void;
   width: number;
   height: number;
-  type: 'normal' | 'highLight' | 'lotteryBtn'
+  type: 'normal' | 'highLight' | 'lotteryBtn',
+  prizeUrl: object,
+  prizeImgStyle : object,
+  prizeName: string,
+  nameStyle: object
 }
 
 const LotteryItem: FC<Props> = (props) => {
-  const { url, type, lotteryPress, width, height,  index } = props;
-    let itemDom = null;
-    switch (type) {
-      case 'normal':
-        itemDom = <Image style={{ width, height }} source={{ uri: url }}/>;  
-        break;
-      case 'highLight':
-        itemDom = (
-          <ImageBackground style={{ width, height }} source={{ uri: url }}>
-            <View style={[ItemStyle.highLightBlock, { width, height }]} />
-          </ImageBackground>
-        );
-        break;
-      case 'lotteryBtn':
-        itemDom = (
-          <TouchableHighlight onPress={() => { lotteryPress!(); }}>
-            <Image style={{ width, height }} source={{ uri: url }}/>
-          </TouchableHighlight>
-        );
-        break;
-    }
-    return (
-      <View style={{ width: width + 2, height: height + 2 }} key={index}>
-        {itemDom}
-      </View>
-    )
+  const { url, type, lotteryPress, width, height, index,  prizeUrl, prizeImgStyle, prizeName,nameStyle } = props;
+  let itemDom = null;
+  switch (type) {
+    case 'normal':
+      itemDom = (
+        <ImageBackground source={url} style={{ width, height, alignItems: 'center', justifyContent: 'center' }}>
+          <Image style={prizeImgStyle} source={prizeUrl} />
+          <Text style={nameStyle}>{prizeName}</Text>
+        </ImageBackground>
+      )
+      break;
+    case 'highLight':
+      itemDom = (
+        <ImageBackground source={url} style={{ width, height, alignItems: 'center', justifyContent: 'center' }}>
+          <Image style={prizeImgStyle} source={prizeUrl} />
+          <Text style={nameStyle}>{prizeName}</Text>
+          <View style={[ItemStyle.highLightBlock,{width, height}] }></View>
+        </ImageBackground>
+      );
+      break;
+    case 'lotteryBtn':
+      itemDom = (
+        <TouchableOpacity onPress={() => { lotteryPress!(); }}>
+          <Image style={{ width, height }} source={url} />
+        </TouchableOpacity>
+      );
+      break;
+  }
+  return (
+    <View style={{ width: width + 2, height: height + 2 }} key={index}>
+      {itemDom}
+    </View>
+  )
 }
 
 export default LotteryItem;
